c569f77f0c014302ccece8566bc3e23688f79394
giorgiomartini <jorgeantonio82@googlemail.com>
2020-09-24 22:13:25 +0200

    added loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
-import Sketch from "react-p5";
+// import Sketch from "react-p5";
+import loadable from "@loadable/component"
+
+const Sketch = loadable(() => import("react-p5"))
 
 const between = (x, min, max) =>  x >= min && x <= max
 
