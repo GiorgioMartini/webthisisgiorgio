474db39d47523934c8ee50a9d60563dd7ebcf515
giorgio <jorgeantonio82@gmail.com>
2020-09-25 20:38:59 +0200

    commented p5

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 // import Sketch from "react-p5";
 import loadable from "@loadable/component"
 
-const Sketch = loadable(() => import("react-p5"))
+// const Sketch = loadable(() => import("react-p5"))
 
 const between = (x, min, max) =>  x >= min && x <= max
 
@@ -13,106 +13,106 @@ function isOverGridPoint (mouseX, mouseY, x, y) {
     ? true
     : false
 }
-const Background = (props) => {
-  let canvas
-  let cols = 20
-  let rows = 10
-  let spots = []
-  const windowResized = (p5) => p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
-  const lineThresh = 30
-  let color = "#2b1b69"
-  // let color = "#af3242"
+// const Background = (props) => {
+//   let canvas
+//   let cols = 20
+//   let rows = 10
+//   let spots = []
+//   const windowResized = (p5) => p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
+//   const lineThresh = 30
+//   let color = "#2b1b69"
+//   // let color = "#af3242"
 
-  function Spot(p5, x, y) {
-    let _x = x
-    let _y = y
-    let _r = 2
-    let opacity = 255
-    let hovered = false
+//   function Spot(p5, x, y) {
+//     let _x = x
+//     let _y = y
+//     let _r = 2
+//     let opacity = 255
+//     let hovered = false
 
-    function display() {
-      p5.noStroke()
-      p5.fill(color)
-      p5.ellipse(_x, _y, _r)
-    }
+//     function display() {
+//       p5.noStroke()
+//       p5.fill(color)
+//       p5.ellipse(_x, _y, _r)
+//     }
 
-    function fade() {
-      if (_r >= 0) {
-        _r -= 1
-      } else {
-        hovered = false
-      }
-    }
+//     function fade() {
+//       if (_r >= 0) {
+//         _r -= 1
+//       } else {
+//         hovered = false
+//       }
+//     }
 
-    function flash() {
-        _r = 80
-        hovered = true
-    }
+//     function flash() {
+//         _r = 80
+//         hovered = true
+//     }
 
-    function randomWalk() {
-      _x += p5.random(-1,1)
-      _y += p5.random(-1,1)
-    }
+//     function randomWalk() {
+//       _x += p5.random(-1,1)
+//       _y += p5.random(-1,1)
+//     }
 
-    function values () {
-      return {
-        _x,
-        _y,
-        hovered,
-      }
-    }
+//     function values () {
+//       return {
+//         _x,
+//         _y,
+//         hovered,
+//       }
+//     }
 
-    return {
-      display,
-      flash,
-      randomWalk,
-      values,
-      fade,
-    }
-  }
+//     return {
+//       display,
+//       flash,
+//       randomWalk,
+//       values,
+//       fade,
+//     }
+//   }
 
-  const setup = (p5, canvasParentRef) => {
-    // p5.pixelDensity(1);
-    canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight /*, p5.WEBGL*/).parent(canvasParentRef)
-    const colWidth = (p5.width / cols)
-    const colHeight = (p5.height / rows)
-    canvas.position(0, 0)
-    canvas.style('z-index', '-1')
-    p5.background(0);
-    for (let x = 0; x < cols+1; x++) {
-      for (let y = 0; y < rows+1; y++) {
-        spots.push(Spot(p5, x * colWidth, y * colHeight))
-      }
-    }
-  }
+//   const setup = (p5, canvasParentRef) => {
+//     // p5.pixelDensity(1);
+//     canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight /*, p5.WEBGL*/).parent(canvasParentRef)
+//     const colWidth = (p5.width / cols)
+//     const colHeight = (p5.height / rows)
+//     canvas.position(0, 0)
+//     canvas.style('z-index', '-1')
+//     p5.background(0);
+//     for (let x = 0; x < cols+1; x++) {
+//       for (let y = 0; y < rows+1; y++) {
+//         spots.push(Spot(p5, x * colWidth, y * colHeight))
+//       }
+//     }
+//   }
 
-  const draw = (p5) => {
-    p5.background(0)
-    // p5.translate(-p5.width / 2, -p5.height / 2,0);
-    p5.strokeWeight(3)
-    spots.forEach((s, i) => {
-      let { _x, _y, hovered } = s.values()
-      s.randomWalk()
-      s.display()
-      if (i !== 0 && isOverGridPoint(p5.mouseX, p5.mouseY, _x, _y)) s.flash() 
-      hovered && s.fade()
+//   const draw = (p5) => {
+//     p5.background(0)
+//     // p5.translate(-p5.width / 2, -p5.height / 2,0);
+//     p5.strokeWeight(3)
+//     spots.forEach((s, i) => {
+//       let { _x, _y, hovered } = s.values()
+//       s.randomWalk()
+//       s.display()
+//       if (i !== 0 && isOverGridPoint(p5.mouseX, p5.mouseY, _x, _y)) s.flash() 
+//       hovered && s.fade()
 
-      spots.forEach(ref => {
-        if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
-          p5.stroke(color)
-          p5.line(_x, _y, ref.values()._x, ref.values()._y)
-        }
-      })
+//       spots.forEach(ref => {
+//         if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
+//           p5.stroke(color)
+//           p5.line(_x, _y, ref.values()._x, ref.values()._y)
+//         }
+//       })
 
-    })
-  };
-  return <Sketch windowResized={windowResized} setup={setup} draw={draw} />
-}
+//     })
+//   };
+//   return <Sketch windowResized={windowResized} setup={setup} draw={draw} />
+// }
 
 const IndexPage = () => {
   return (
     <div>
-      <Background />
+      {/* <Background /> */}
       <Layout>
         <div className="max-w-lg">
           <p className="mt-16 mb-4 text-6xl">Hey!</p>
